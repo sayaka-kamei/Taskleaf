b3566beb6b5a14e32dc9c89d5c69bb5904f3da77
sayaka kamei <sayukou316@gmail.com>
2022-10-25 01:09:58 +0900

    詳細画面と一覧画面を作成した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,29 @@
 class TasksController < ApplicationController
+
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
