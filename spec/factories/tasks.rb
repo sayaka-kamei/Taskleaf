e9a8e34cde66765dabe6426a9cc38a9c78983627
sayaka kamei <sayukou316@gmail.com>
2022-10-26 11:06:18 +0900

    System Specを修正した

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :task do
-    
+    name { 'test_name1' }
+    description { 'test_description1' }
+  end
+
+  factory :second_task, class: Task do
+    name { 'test_name2' }
+    description { 'test_description2' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
+    FactoryBot.create(:task, name: 'test_name1')
+    FactoryBot.create(:task, name: 'test_name1')
+    FactoryBot.create(:second_task, name: 'test_name2', description: 'test_leaf2')
   end  
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-        task = FactoryBot.create(:task)
         visit new_task_path
-        fill_in "task[name]",with: "task"
+        fill_in "task[name]", with: "task"
+        fill_in "task[description]", with: "task_leaf"
         click_on "登録する"
         expect(page).to have_content 'task'
       end
@@ -18,7 +19,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, name: 'task')
+        task = FactoryBot.create(:task, name: 'task', description: 'task_leaf')
         visit tasks_path
         expect(page).to have_content 'task'
       end
@@ -27,7 +28,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        @task = FactoryBot.create(:task, name: 'task')
+        @task = FactoryBot.create(:task, name: 'task', description: 'task_leaf')
         visit task_path(@task.id)
         expect(page).to have_content 'task'
       end
