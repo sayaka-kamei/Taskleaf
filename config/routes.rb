532155ae2f40c33d40511407e66551f7825de39e
sayaka kamei <sayukou316@gmail.com>
2022-11-01 18:01:57 +0900

    ログイン機能を作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  root to: 'users#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :tasks do
     collection do
       post :confirm
