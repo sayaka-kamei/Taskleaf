308dec10f12fb54d379f5d4785508cf9f17607d4
sayaka kamei <sayukou316@gmail.com>
2022-11-06 00:32:50 +0900

    モデルを修正した

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   private
 
